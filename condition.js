3da36d990a9c8438b4a98481feabf4073949ba0e
SadikRa <sadikrahman494@gmail.com>
2024-11-27 18:40:49 +0600

    nested condition done

---
+++ condition.js
@@ -0,0 +1,12 @@
+const penPrice = 5 ;
+const paperPrice = 5 ;
+
+if(penPrice < paperPrice){
+    console.log("ami paper don't want t0 buy");
+}else if(penPrice > paperPrice){
+    console.log("I want to buy paper");
+}else if(penPrice == paperPrice){
+    console.log("i want to buy both");
+}else{
+    console.log('ami poor I don"t have money');
+}
---
+++ nested.js
@@ -0,0 +1,11 @@
+const money = 500;
+
+if(money > 300) {
+    console.log('tui to boro lok person');
+}else{
+    if(money > 100){
+        console.log('tui boro lok nah tui gorib o nah');
+    }else if(money > 0){
+        console.log('tui kola kah');
+    }
+}
