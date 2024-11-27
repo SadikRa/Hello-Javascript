510cd1bb2187b6b90a3adf5a88e0d4c19af6f62f
SadikRa <sadikrahman494@gmail.com>
2024-11-27 12:22:26 +0600

    comparison done

---
+++ comp.js
@@ -0,0 +1,17 @@
+// comparison 
+
+// 1. bigger : >
+// 2. less: <
+// 3: equal: ==
+// 4. greater than or equal: >=
+// 5. less than or equal: <=
+// 6. not equal = !=
+
+// 7. and: && 
+// 8. or:  ||
+
+
+// console.log(5 <= 10 );
+// console.log(5 == 10 );
+console.log(10 == 10 );
+
--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-// Function to reverse a string
-function reverseString(str) {
-    // Split the string into an array of characters, reverse it, and join it back
-    return str.split('').reverse().join('');
-  }
+// // Function to reverse a string
+// function reverseString(str) {
+//     // Split the string into an array of characters, reverse it, and join it back
+//     return str.split('').reverse().join('');
+//   }
   
-  // Test the function
-  const input = "Hello, World!";
-  const reversed = reverseString(input);
+//   // Test the function
+//   const input = "Hello, World!";
+//   const reversed = reverseString(input);
   
-  console.log("Original String:", input);    // Output: "Hello, World!"
-  console.log("Reversed String:", reversed); // Output: "!dlroW ,olleH"
+//   console.log("Original String:", input);    // Output: "Hello, World!"
+//   console.log("Reversed String:", reversed); // Output: "!dlroW ,olleH"
   
+//number
+//  var sunglassPrice = 423;
+
+//  //string
+//  var subject = 'ba';
+
+//  //bollean;
+
+//  var passed = true;
+
+
+//  var lastExam = 54;
+
+
+
+
