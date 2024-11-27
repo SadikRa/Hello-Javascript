1c7a54cab841103409f4989c7fd00e4ec3648417
SadikRa <sadikrahman494@gmail.com>
2024-11-27 22:13:18 +0600

    ternary done

---
+++ ternary.js
@@ -0,0 +1,15 @@
+
+
+// const age = 20 ;
+// if(age >= 20){
+//     console.log('go and vote');
+// }
+
+const age = 20 ;
+
+// age >= 18 ? console.log('vote da') : console.log("vot dibi nah");
+
+let price = 500;
+const isLeader = true;
+
+price = isLeader === true ? 0 : price + 100;
