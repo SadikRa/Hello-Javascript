19e38c22a9f49f34e253fd41748d329af0376309
SadikRa <sadikrahman494@gmail.com>
2025-09-16 16:36:07 +0600

    jor bizor done

--- loop.js
+++ loop.js
@@ -73,17 +73,30 @@ for (let i = 20; i <= 50; i++) {
   }
 }
 
-
-for(let i = 0; i < 15; i++) {
-// console.log(i);
-if(i >= 7) {
-break;
+for (let i = 0; i < 15; i++) {
+  // console.log(i);
+  if (i >= 7) {
+    break;
+  }
 }
+
+for (let i = 0; i < 10; i++) {
+  if (i == 6) {
+    continue;
+  }
+  // console.log(i);
 }
 
-for(let i = 0 ; i < 10; i ++){
-  if(i == 6 ){
-    continue
+for (let i = 0; i < 10; i++ ) {
+  if (i % 2 == 0) {
+    continue;
+  }
+  // console.log(i);
+}
+
+for (let i = 0; i < 10; i++ ) {
+  if (i % 2 == 1) {
+    continue;
   }
   // console.log(i);
 }
