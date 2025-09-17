0d4e4a6f04a4dbda543311c6ff27bddbd5a1850e
SadikRa <sadikrahman494@gmail.com>
2025-09-17 15:32:58 +0600

    updated

--- function.js
+++ function.js
@@ -8,4 +8,18 @@ function sum(sum1, sum2) {
   console.log(sum1 + sum2);
 }
 
-sum(2, 3);
+// sum(2, 3);
+
+function sayHello(name = "Guest") {
+  console.log(`Hello, ${name}!`);
+}
+// sayHello(); // Output: "Hello, Guest!"
+// sayHello("Bob"); // Output: "Hello, Bob!"
+
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(doubled);
+}
+
+// doubleIt(12)
+
