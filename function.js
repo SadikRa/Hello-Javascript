d187c13615ff7093ed463e07cc7e3a07a4de635c
SadikRa <sadikrahman494@gmail.com>
2025-09-17 15:49:27 +0600

    updated

--- function.js
+++ function.js
@@ -16,10 +16,23 @@ function sayHello(name = "Guest") {
 // sayHello(); // Output: "Hello, Guest!"
 // sayHello("Bob"); // Output: "Hello, Bob!"
 
-function doubleIt(number){
-    const doubled = number * 2;
-    console.log(doubled);
+function doubleIt(number) {
+  const doubled = number * 2;
+  console.log(doubled);
 }
 
 // doubleIt(12)
 
+function addNumbers(a, b) {
+  console.log(arguments);
+  return a + b;
+}
+const result = addNumbers(5, 8, 9, 6);
+// console.log(result);
+
+function addAll(a, b, c, d, e, f) {
+  return a + b + c + d + e + f;
+}
+
+const result1 = addAll(1, 2, 3, 4, 5, 6);
+console.log(result1);
