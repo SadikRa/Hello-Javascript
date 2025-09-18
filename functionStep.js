ab503a484b4bcaf4d2ea8e543f1535fc9f72927d
SadikRa <sadikrahman494@gmail.com>
2025-09-18 20:24:07 +0600

    updated

--- functionStep.js
+++ functionStep.js
@@ -130,4 +130,21 @@ function oddAverage(numbers) {
   return average;
 }
 
-console.log(oddAverage([1, 2, 23, 4, 23, 42, 3]));
+// console.log(oddAverage([1, 2, 23, 4, 23, 42, 3]));
+
+const students = ["abul", "babul", "cabul", "abul", "babul", "dabul"];
+const numbers = [1, 5, 61, 5, 87, 7, 5, 81, 61];
+
+function noDuplicate(array) {
+  const unique = [];
+
+  for (const item of array) {
+    if (unique.includes(item) === false) {
+      unique.push(item);
+    }
+  }
+  return unique;
+}
+
+const uniqueArray = noDuplicate(students);
+console.log(uniqueArray);
