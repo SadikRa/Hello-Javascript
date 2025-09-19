b5091fed51c137b9282fc1d35c23ee392ec50626
SadikRa <sadikrahman494@gmail.com>
2025-09-19 07:23:42 +0600

    updated

--- es6.js
+++ es6.js
@@ -1,1 +1,16 @@
 const arrow = (a, b) => a + b;
+
+
+const person = {
+name: "Ananta",
+greetRegular: function () {
+console.log("Hello, " + this.name);
+},
+greetArrow: () => {
+console.log("Hello, " + this.name);
+}
+};
+
+// person.greetRegular();
+// person.greetArrow();
+
