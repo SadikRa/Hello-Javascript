7a7665c63956394e2da4c6077a0a5961312b1de3
SadikRa <sadikrahman494@gmail.com>
2025-09-19 11:10:09 +0600

    updated rest

--- es6.js
+++ es6.js
@@ -1,16 +1,19 @@
 const arrow = (a, b) => a + b;
 
-
 const person = {
-name: "Ananta",
-greetRegular: function () {
-console.log("Hello, " + this.name);
-},
-greetArrow: () => {
-console.log("Hello, " + this.name);
-}
+  name: "Ananta",
+  greetRegular: function () {
+    console.log("Hello, " + this.name);
+  },
+  greetArrow: () => {
+    console.log("Hello, " + this.name);
+  },
 };
 
 // person.greetRegular();
 // person.greetArrow();
 
+const [first, second, ...rest] = [10, 20, 30, 40, 50];
+// console.log(first);
+// console.log(second);
+// console.log(rest);
