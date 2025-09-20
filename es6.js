8f4187e04fe98a0b692031697c269c343924ff09
SadikRa <sadikrahman494@gmail.com>
2025-09-20 21:20:35 +0600

    filter added done

--- es6.js
+++ es6.js
@@ -22,16 +22,33 @@ const numbers = [3, 5, 2, 45, 5, 43, 90, 32, 15];
 const arrayMax = Math.max(...numbers);
 // console.log(arrayMax);
 
-
-function doubleAll(numbers){
+function doubleAll(numbers) {
   const doubled = [];
-  for(const num of numbers){
+  for (const num of numbers) {
     const value = num * 2;
-    doubled.push(value)
+    doubled.push(value);
   }
-  return doubled
+  return doubled;
 }
 
 const numbers1 = [3, 5, 2, 45, 5, 43, 90, 32, 15];
 // console.log(doubleAll(numbers1));
+
+const numbers2 = [3, 5, 2, 45, 5, 43, 90, 32, 15];
+
+const result = numbers2.map((num) => num * 2);
+
+// console.log(result);
+
+const friends = ["Tom", "Jhon", "Micheal", "Oliver"];
+const lengths = friends.map((frnd) => frnd.length);
+const firstLetters = friends.map((friend) => friend[0]);
+// console.log(lengths);
+// console.log(firstLetters);
+
+// Output: [3, 4, 7, 6][("T", "J", "M", "O")];
+
+const friends1 = ["tom", "john", "micheal", "oliver", "tim", "joshna"];
+const oldFriends = friends1.filter((friend) => friend.length > 4);
+// console.log(oldFriends);
 
