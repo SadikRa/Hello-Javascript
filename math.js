57fde5b96cdd170da3f3e633aebc95a8be5f6c8d
SadikRa <sadikrahman494@gmail.com>
2025-09-25 08:12:03 +0600

    updated

--- math.js
+++ math.js
@@ -17,3 +17,19 @@ const max = Math.max(45, 21, 65, 99, 126, 5, -99);
 
 // console.log(Math.random());
 // console.log(Math.random());
+
+const specificDate = new Date("2032-05-21");
+// console.log(specificDate);
+
+const date1 = new Date("2002-06-21");
+const date2 = new Date("2025-09-25");
+
+const diffInMiliSeconds = date2 - date1;
+const diffInDays = diffInMiliSeconds / (1000 * 60 * 60 * 24);
+// console.log(diffInDays);
+
+const sentence = "i have a apple";
+
+const pattern1 = /apple/;
+const pattern = new RegExp("apple");
+console.log(pattern.test(sentence));
