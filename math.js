df536fe363eb3e2cf5bbd823859a328f6ed04b3a
SadikRa <sadikrahman494@gmail.com>
2025-09-28 12:19:12 +0600

    updated

--- math.js
+++ math.js
@@ -37,4 +37,12 @@ console.log(pattern.test(sentence));
 // set
 
 const mySet = new Set([1, 2, 3, 4, 5]);
-console.log(mySet);
+// console.log(mySet);
+
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+const uniqueNumbers = new Set(numbers);
+// console.log(uniqueNumbers);
+
+const numberSet = [2, 2, 3, 3, 4, 4, 5, 6];
+const uniqueSet = new Set(numberSet);
+console.log(uniqueSet);
