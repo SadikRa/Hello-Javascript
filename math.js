c97af6606ed01b793f846da238cd0e6ddfe5ee1c
SadikRa <sadikrahman494@gmail.com>
2025-09-30 21:10:35 +0600

    updated

--- math.js
+++ math.js
@@ -63,3 +63,13 @@ mySet.add(30);
 mySet.delete(10);
 // console.log(mySet);
 
+function multiply(num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "Please provide a number";
+  }
+  const mult = num1 * num2;
+  return mult;
+}
+
+const result = multiply(5, 7);
+// console.log(result);
