e2c7f492082b54ede028f6d7ee753814fade9d77
SadikRa <sadikrahman494@gmail.com>
2025-12-12 20:43:32 +0600

    find added done

--- 25.js
+++ 25.js
@@ -1,9 +1,10 @@
-
-
 const numbers = [12, 23, 4, 4, 55, 56, 40, 23, 32, 32, 3, 2, 3];
 
 const filters = numbers.filter((numbers) => numbers < 40);
 
 // console.log(filters);
 
-const finds = numbers
+const finds = numbers.find((numbers) => numbers == 40);
+
+// console.log(finds);
+
