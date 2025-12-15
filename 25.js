c187b336ec268e0eba43b77f78e3729522cebfcc
SadikRa <sadikrahman494@gmail.com>
2025-12-15 07:48:00 +0600

    for in loopp added done

--- 25.js
+++ 25.js
@@ -29,17 +29,53 @@ const sum2 = numbers.reduce(
 /// TO DO https://www.youtube.com/watch?v=bAUMuuRH99o
 
 const price = [12, 2, 3, 4, 5, 5, 6, 55, 33];
-const sum3 = price.reduce((accumulator, currentValue) => {
-  return accumulator + currentValue;
+const sum3 = price.reduce((s1, s2) => {
+  return s1 + s2;
 }, 0);
 
 // console.log(sum3);
 
 price.map((value) => {
   // console.log(value);
-})
+});
 
 const fil = price.filter((p) => {
- return p== 12
-})
+  return p == 12;
+});
 // console.log(fil);
+
+for (let i = 0; i < 10; i++) {
+  // console.log(i);
+}
+
+for (key of price) {
+  // console.log(key);
+}
+
+let sum4 = 0;
+
+for (sum4; sum4 < 10; sum4++) {
+  // console.log(sum4);
+}
+
+// let sum5 = 0;
+// let n = prompt("give me a value n");
+// n = Number.parseInt(n);
+
+// for (let i = 0; i < n; i++) {
+//   sum5 += (i + 1);
+// }
+// console.log("sum first" + n + "natural number" + sum5);
+
+let companion = {
+  AbuBkar: 1,
+  Omar: 2,
+  Utsman: 3,
+  Ali: 4,
+};
+
+
+// for of loop
+for (let value in companion) {
+  // console.log(" status of " + value + " is " + companion[value]);
+}
