8a85eddbc568df4f789b49bed211224c6c5fa039
SadikRa <sadikrahman494@gmail.com>
2025-12-31 15:00:49 +0600

    reduce okh

---
+++ theEnd_25.js
@@ -0,0 +1,7 @@
+/// okh let start ;
+const numbers = [12, 23, 4, 4, 55, 56, 40, 23, 32, 32, 3, 2, 3];
+
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+});
+// console.log(sum);
